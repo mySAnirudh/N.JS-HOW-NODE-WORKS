1f4311d98132f3614df7f65e8981144135e85f78
Anirudh Kumar <96437957+mySAnirudh@users.noreply.github.com>
2022-04-16 00:19:14 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("This is frist line")
+
+console.log("This is frist line 2")
+setTimeout(()=>{
+    console.log("This is frist line 3")
+},2000)
+
+setTimeout(()=>{
+    console.log("This is frist line 4")
+},0
+)
+console.log("This is frist line 5")
+
+console.log("This is frist line 6")
+
+// This is the Bahavious of node js 
+// Try out in terminal 
